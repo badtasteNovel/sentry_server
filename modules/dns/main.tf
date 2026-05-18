fb419af5a1924eec0159607368e7c63c09ad0932
badtastenovel <stones4586@gmail.com>
2026-05-18 09:05:36 +0800

    feat: delete tofu 留下 seed.iso.sh

--- modules/dns/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "sentry" {
-  zone_id = var.zone_id
-  name    = var.domain_name
-  type    = "A"
-  ttl     = 300
-  records = [var.public_ip]
-}
--- modules/security_group/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "id" {
-  value = aws_security_group.sentry.id
-}
-
-output "name" {
-  value = aws_security_group.sentry.name
-}
